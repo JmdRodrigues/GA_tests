c690e33db7b02ac90c465d53c37ed75a9643e9d1
JmdRodrigues <rjoao06@gmail.com>
2018-10-09 18:23:11 +0100

    added first working algorithm

--- plotTools.py
+++ plotTools.py
@@ -39,32 +39,143 @@ def findcloser(array, value):
 	idx = (np.abs(array-value)).argmin()
 	return idx
 
-
-def createSquareMatrix(ncols, nrows, post_colors, force_colors, stations, ind):
-
+def createSquareMatrix(ncols, nrows, all_colors, stations, ind, score_per_seq, Circles=False):
 	inbetween = 0.1
-	wid = 1
+	wid = 5
 	hei = 1
-	xx = np.arange(0, ncols, (wid + inbetween))
-	yy = np.arange(0, nrows, (hei + inbetween))
+	xx = np.arange(0, wid*ncols, (wid + inbetween))
+	yy = np.arange(0, hei*nrows + 1, (hei + inbetween))
 
-
+	fig = plt.figure(figsize=(100, 100))
 	ax = plt.subplot(111, aspect='equal')
 	for xi, x in zip(xx, range(0, ncols)):
-		for yi, y in zip(yy, range(0, nrows)):
-			sq = patches.Rectangle((xi, yi), wid, hei, fc='lightsteelblue', alpha=0.5)
-			cr_post = patches.Circle((xi+wid / 4, yi+hei / 2), wid/6, fc=post_colors[y][x], alpha=0.5)
-			cr_force = patches.Circle((xi + 3*wid / 4, yi + hei / 2), wid / 6, fc=force_colors[y][x],alpha=0.7)
+		ax.text(xi+wid/4, nrows+hei+1, "Rot "+str(x))
+		for yi, y in zip(reversed(yy), range(0, nrows)):
+			sq = patches.Rectangle((xi, yi), wid, hei, fc=all_colors["Score_total_estacao"][ind[y][x]-1], alpha=1)
 			ax.add_patch(sq)
-			ax.add_patch(cr_post)
-			ax.add_patch(cr_force)
-			ax.text(xi+wid/2-wid/10, yi+3*hei/4, stations[ind[y][x]-1])
+			if(Circles == True):
+				for index, key in enumerate(all_colors.keys()):
+					cr_post = patches.Circle((xi + 0.5 + 1.3*index/5, yi+hei / 2), 1/7, fc=all_colors[key][ind[y][x]-1], alpha=0.5)
+					ax.add_patch(cr_post)
+				# cr_post2 = patches.Circle((xi + 2 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_Bent"][ind[y][x]-1], alpha=0.5)
+			# cr_post3 = patches.Circle((xi + 3 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_twisted_lateral_bent"][ind[y][x]-1], alpha=0.5)
+			# cr_post4 = patches.Circle((xi + 4 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%Shoulder height"][ind[y][x]-1], alpha=0.5)
+			# cr_post5 = patches.Circle((xi + 5 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_head_level"][ind[y][x]-1], alpha=0.5)
+			# cr_post6 = patches.Circle((xi + 6 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_range60"][ind[y][x]-1], alpha=0.5)
+			# cr_post7 = patches.Circle((xi + 7 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_range80"][ind[y][x]-1], alpha=0.5)
+			# cr_post8 = patches.Circle((xi + 8 / 5, yi + hei / 2), 1 / 10, fc=all_colors["%_range100"][ind[y][x]-1], alpha=0.5)
+			# cr_post9 = patches.Circle((xi + 9 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Force_Score"][ind[y][x]-1], alpha=0.5)
+			# cr_post10 = patches.Circle((xi + 10 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Force_light"][ind[y][x]-1], alpha=0.5)
+			# cr_post11 = patches.Circle((xi + 11 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Force_midddle"][ind[y][x]-1], alpha=0.5)
+			# cr_post12 = patches.Circle((xi + 12 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Force_hard"][ind[y][x]-1], alpha=0.5)
+			# cr_post13= patches.Circle((xi + 13 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Force_stressed"][ind[y][x]-1], alpha=0.5)
+			# cr_post14 = patches.Circle((xi + 14 / 5, yi + hei / 2), 1 / 10, fc=all_colors["Score_vibrations"][ind[y][x]-1], alpha=0.5)
+
+
+			# ax.add_patch(cr_post2)
+			# ax.add_patch(cr_post6)
+			# ax.add_patch(cr_post9)
+			# ax.add_patch(cr_post14)
+			ax.text(xi+wid/2-wid/6, yi+2*hei/3, stations[ind[y][x]-1])
+			ax.text(-1.2,yi+hei/2, "Wkr" + str(y))
+			ax.text(wid*ncols+1.2, yi+hei/2, str(score_per_seq[y]))
+		ax.text(wid*ncols, -0.5, "Final Score:" + str(np.sum(score_per_seq)))
 
 	plt.axis('off')
-	ax.axis([0, ncols+1,0,nrows+1])
+	ax.axis([0, wid*ncols+1,0,nrows+1])
+	# plt.show()
+
+
+def createWorkplaceMatrix(risk_factors):
+
+	inbetween = 0.1
+	wid = 2
+	hei = 1
+	ncols = len(list(risk_factors.keys())[3:])
+	nrows = len(risk_factors["URQ"])
+	stations = risk_factors["Estação"]
+	risk_factors_tags = ["P_S", "%B", "%SB", "%TB", "%SH", "%HL", "%R6", "%R8", "%RT", "FS", "FL", "FM", "FH", "FS", "SV", "LS", "OS"]
+	xx = np.arange(0, 2*ncols, (wid + inbetween))
+	yy = np.arange(0, nrows, (hei + inbetween))
+	ax = plt.subplot(111, aspect='equal')
+
+	colors_factors = {}
+
+	tag = 0
+	for key, xi in zip(list(risk_factors.keys())[4:], xx):
+		col = convert2color(key, risk_factors[key])
+		colors_factors[key] = col
+		for y, yi in zip(range(0, len(col)), yy):
+			sq = patches.Rectangle((xi, yi), wid, hei, fc=col[y], alpha=0.5)
+			ax.add_patch(sq)
+			# add text of workplace
+		ax.text(xi+wid/8, 12 * hei + 1, risk_factors_tags[tag], size=14)
+		tag+=1
+
+
+	#for the overall score:
+	col = convert2color("Score_total_estacao", risk_factors["Score_total_estacao"])
+	colors_factors["Score_total_estacao"] = col
+	for y, yi in zip(range(0, len(col)), yy):
+		sq_w = patches.Rectangle((max(xx) + 1, yi), wid, hei, fc="white", alpha=0.5)
+		ax.add_patch(sq_w)
+		sq = patches.Rectangle((max(xx)+2, yi), wid, hei, fc=col[y], alpha=0.5)
+		ax.add_patch(sq)
+		ax.text(-1.8 * wid, yi + hei / 4, stations[y])
+	ax.text(wid*ncols + 2, 12 * hei + 1, risk_factors_tags[-1], size=14)
+
+	plt.axis('off')
+	ax.axis([0, 2*(ncols+3), 0, nrows+2])
 	plt.show()
 
+	return colors_factors
 
 
+def convert2color(key, col):
+	green = (60, 179, 113)
+	gold = (255,215,0)
+	red = (178, 34, 34)
 
+	if(key == "Score_total_estacao"):
+		max_s = 80
+		min_s = 30
 
+		ll3_5 = np.linspace(30, 50, 50000)
+		color1 = gradient(red, gold, 50000)
+		color2 = gradient(gold, green, 50000)
+		color = color2 + color1
+		# print(len(color))
+		colors = []
+		for s in col:
+			if(s < 30):
+				colors.append(color2[0])
+			elif(s >30 and s<50):
+				colors.append(color1[findcloser(ll3_5, s)])
+			else:
+				colors.append(color1[-1])
+	else:
+		if(key == "Postura_score"):
+			max_s = 50
+			min_s = 0
+		elif(key == "Force_Score"):
+			max_s = 50
+			min_s = 0
+		elif(key == "Score_vibrations"):
+			max_s = 4
+			min_s = 0
+		elif("%" in key):
+			max_s = 100
+			min_s = 0
+		else:
+			min_s = 0
+			max_s = 1
+
+		ll = np.linspace(min_s, max_s, 100000)
+		color1 = gradient(red, gold, 50000)
+		color2 = gradient(gold, green, 50000)
+		color = color2+color1
+		# print(len(color))
+
+		colors = [color[findcloser(ll, s)] for s in col]
+
+	return colors
--- urqa4_main.py
+++ urqa4_main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-from GA_tests.plotTools import gradient, findcloser, createSquareMatrix
+from GA_tests.plotTools import gradient, findcloser, createSquareMatrix, createWorkplaceMatrix
 import matplotlib.pyplot as plt
 import random
+import time
 
 """
 
@@ -10,16 +11,31 @@ import random
 2 - calculate a fitness value for an example of sequences
 
 """
+def fitness(ind, stations, wp_dict):
+	val = []
+	for worker in ind:
+		seq = [stations[w-1] for w in worker]
+		score = fitnessSeq(seq, wp_dict)
+		val.append(score)
+	# print(val)
+	ind_score = sum(val)
+	return val, ind_score
+
 def fitnessSeq(wp_seq, wp_dict):
-	keys = ['p_s', 'f_s']
+	keys = ["P_S", "%B", "%SB", "%TB", "%SH", "%HL", "%R6", "%R8", "%RT", "FS", "FL", "FM", "FH", "FS", "SV", "LS", "OS"]
+	weight = [10, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 10, 0, 0]
 	seq_temp = []
 	for key in keys:
 		seq_k = np.array([wp_dict[i][key] for i in wp_seq])
 		seq_temp.append(fitnessCalc(seq_k))
-	return np.sum(seq_temp)
+	return np.sum(np.multiply(weight, seq_temp))
 
 def fitnessCalc(seq):
-	DP = [1, 4, 2, 3]
+	cumulation = [1, 3, 2, 4]
+	duration = [0.23, 0.38, 0.19, 0.20]
+
+	DP = np.multiply(cumulation, duration)
+	# DP = duration
 	We = 100
 
 	Wta = 10
@@ -27,9 +43,9 @@ def fitnessCalc(seq):
 	we_e = We * seq
 	wta_ta = variability(seq, Wta)
 
-	t = np.sum((DP * we_e) + wta_ta)
+	t = np.sum((DP*we_e) + wta_ta)
 
-	return t
+	return np.exp(t/100)
 
 def variability(seq, w):
 	dseq = np.diff(seq)
@@ -77,19 +93,24 @@ def createIndividual(Nshifts, Nworkers):
 # def FitnessFunction():
 
 
-def crossover(ind1, ind2, nworkers, type):
+def crossover(ind1, ind2, nworkers, type="skip_row"):
 	#mating step
 	if type is "skip_row":
 		"""
 			With this each row is sequentially shifted between the two individuals
 		"""
-		skiprow(ind1, ind2, nworkers)
+		mat1, mat2 = skiprow(ind1, ind2, nworkers)
+		mat1 = errorCorrection(mat1)
+		mat2 = errorCorrection(mat2)
+
+		return mat1, mat2
 
 	# elif type is "point_row":
 
-def TournamentSelection(pop, k):
+def TournamentSelection(pop, k, n):
 	"""
 	:param k: tournament size
+	:param n: number of winners
 	:param pop: entire population
 	:return: matrix that won the tournament
 	"""
@@ -97,27 +118,41 @@ def TournamentSelection(pop, k):
 	if k > len(pop):
 		print("the tournament pool size should be lower than the population size")
 	else:
-		tournamentSel = random.sample(range(0, len(pop)), k)
+		winners = [0, 0]
+		while(np.multiply(winners[0], winners[1]) != 0):
+			tournamentSel = random.sample(range(0, len(pop)), k)
+			winners = [0, 0]
+			previous_score = 0
+			for round in range(0, n):
+				min_score = 100000000
+				for candidate in tournamentSel:
+					ind_score = pop[candidate]['score']
+					if(np.logical_and(ind_score < min_score, ind_score > previous_score)):
+						min_score = ind_score
+						winners[round] = candidate
+						previous_score = min_score
 
-		tournamentPop = pop[tournamentSel]
+				tournamentSel.remove(winners[round])
+
+		return winners
 
 		#Compete the elements of the population
 		# winner = the one with the higher score
 
 def skiprow(mat1, mat2, Nworkers):
 
-	child1 = mat1
-	child2 = mat2
+	child1 = mat1["ind"]
+	child2 = mat2["ind"]
 	divider = Nworkers // 4
 	# mating process--------------------------
 	# child1--------------
-	child1[:divider, :] = mat2[:divider, :]
-	child1[divider:3 * divider, :] = mat2[divider:3 * divider, :]
-	child1[3 * divider:, :] = mat2[3 * divider:, :]
+	child1[:divider][:] = child2[:divider][:]
+	child1[divider:3 * divider][:] = child2[divider:3 * divider][:]
+	child1[3 * divider:][:] = child2[3 * divider:][:]
 	# child2--------------
-	child2[:divider, :] = mat1[:divider, :]
-	child2[divider:3 * divider, :] = mat1[divider:3 * divider, :]
-	child2[3 * divider:, :] = mat1[3 * divider:, :]
+	child2[:divider][:] = mat1['ind'][:divider][:]
+	child2[divider:3 * divider][:] = mat1['ind'][divider:3 * divider][:]
+	child2[3 * divider:][:] = mat1['ind'][3 * divider:][:]
 
 	return child1, child2
 
@@ -173,21 +208,38 @@ def errorCorrection(mat):
 
 def load_scores(): #load apergo data - Fatores de risco AP ergo
 	apergo = pd.read_excel("URQA4/Fatores_risco_APErgo_URQA4.xlsx")
-	wp_dict = {}
+
 	stations = apergo["Estação"].to_dict()
-	print(stations)
+
 	posture_score = apergo["Postura_score"].to_dict()
 	force_score = apergo["Force_Score"].to_dict()
+	vibration_score = apergo["Score_vibrations"].to_dict()
+	key_tags = ["OS", "P_S", "%B", "%SB", "%TB", "%SH", "%HL", "%R6", "%R8", "%RT", "FS", "FL", "FM", "FH", "FS", "SV", "LS"]
+
+	wp_dict = {station:{tag:0 for tag in key_tags} for station in stations.values()}
+
+	tag_dict = {}
+
+	#normalize vectors
+	for tag, key in zip(key_tags, apergo.keys()[3:]):
+		mn = apergo[key].mean()
+		std = apergo[key].std()
+		if(std == 0):
+			score_list = np.array(list(apergo[key]))
+		else:
+			score_list = (np.array(list(apergo[key])) - mn)/std
+		tag_dict[tag] = score_list
+
 
 	for i in range(0, len(stations)):
-		wp_dict[stations[i]] = {'p_s': posture_score[i],
-								'f_s': force_score[i]
-								}
-	return wp_dict, stations, posture_score, force_score
+		for tag in key_tags:
+			wp_dict[stations[i]][tag] = tag_dict[tag][i]
+
+
+	return wp_dict, stations, posture_score, force_score, vibration_score, apergo
 
 def createScoreMatrixANDColor(score, ind):
 	"""
-
 	:param score: score dictionary
 	:param ind: individual, represented as a matrix with sequences in each row
 	:return: matrix with all the scores
@@ -196,12 +248,14 @@ def createScoreMatrixANDColor(score, ind):
 	clr_mat = []
 
 	green = (60, 179, 113)
+	gold = (255, 215, 0)
 	red = (178, 34, 34)
 
 	ll = np.linspace(min(score.values()), max(score.values()), 100000)
-	color = gradient(red, green, 100000)
+	color1 = gradient(red, gold, 50000)
+	color2 = gradient(gold, green, 50000)
+	color = color2+color1
 
-	print(score)
 	for seq in ind:
 		seq_i =  [score[ws-1] for ws in seq]
 		scr_mat.append(seq_i)
@@ -211,24 +265,199 @@ def createScoreMatrixANDColor(score, ind):
 
 	return scr_mat, clr_mat
 
+def fitnesspop(pop, stations, wp_dict):
+	for index in range(0, len(pop)):
+		seq_scores, ind_score = fitness(pop[index]['ind'], stations, wp_dict)
+		# print(seq_scores)
+		pop[index]['score'] = ind_score
 
-wp_dict, stations, post_score, force_score = load_scores()
+
+	return pop
+
+def newpop(mat1, mat1_score, mat2, mat2_score, poplt):
+	max_array = []
+	keys = list(poplt.keys())
+	n = 0
+	while(n < 2):
+		max_score=0
+		for index, key in enumerate(keys):
+			ind_score = poplt[key]['score']
+			if(ind_score > max_score):
+				max_score = ind_score
+				maxi = index
+
+		max_array.append(keys[maxi])
+		keys.remove(keys[maxi])
+		n+=1
+
+	if mat2_score < poplt[max_array[1]]['score']:
+		poplt[max_array[1]].update({'ind':mat2, 'score':mat2_score})
+		poplt[max_array[0]].update({'ind': mat1, 'score': mat1_score})
+	else:
+		if mat1_score < poplt[max_array[1]]['score']:
+			poplt[max_array[1]].update({'ind': mat1, 'score': mat1_score})
+
+			if(mat2_score < poplt[max_array[0]]['score']):
+				poplt[max_array[0]].update({'ind': mat2, 'score': mat2_score})
+
+	return poplt
+
+def meanScores(pop):
+	sum_s = 0
+	for key in pop.keys():
+		sum_s += pop[key]['score']
+
+	return sum_s/len(pop)
+
+def get_best_pop(poplt):
+	keys = list(pop.keys())
+	max_score = 0
+	for index, key in enumerate(keys):
+		ind_score = poplt[key]['score']
+		if (ind_score > max_score):
+			max_score = ind_score
+			maxi = index
+	return keys[maxi]
+
+def get_best_pop2(poplt):
+	keys = list(pop.keys())
+	max_score = 100000
+	for index, key in enumerate(keys):
+		ind_score = poplt[key]['score']
+		if (ind_score < max_score):
+			max_score = ind_score
+			maxi = index
+	return keys[maxi]
+
+def load_TL_example():
+	# example matrix from Team Leader
+
+	code_dict = dict(A=1, B=3, C=4, D=2, E=5, F=6, H=7, I=8, J=9, K=10, L=11,
+					 M=12)
+
+	tl_matrix = [['A', 'K', 'B', 'I'],
+				 ['E', 'D', 'J', 'A'],
+				 ['C', 'J', 'E', 'H'],
+				 ['B', 'M', 'L', 'E'],
+				 ['K', 'B', 'M', 'F'],
+				 ['J', 'C', 'H', 'M'],
+				 ['D', 'I', 'K', 'B'],
+				 ['M', 'L', 'F', 'D'],
+				 ['L', 'H', 'C', 'J'],
+				 ['H', 'F', 'A', 'K'],
+				 ['I', 'E', 'D', 'C'],
+				 ['F', 'A', 'I', 'L'], ]
+
+	ind_tl = [[code_dict[tl_matrix[j][i]] for i in range(0, 4)] for j in range(0, 12)]
+
+	return ind_tl
+#.....................................................................
+wp_dict, stations, post_score, force_score, vibration_score, risk_factors = load_scores()
+
+# print(wp_dict)
 
 ind = createIndividual(4, 12)
-post_mat, post_color = createScoreMatrixANDColor(post_score, ind)
-force_mat, force_color = createScoreMatrixANDColor(force_score, ind)
+score_per_seq, ind_score = fitness(ind, stations, wp_dict)
+score_per_seq_tl, ind_score_tl = fitness(load_TL_example(), stations, wp_dict)
 
-print(post_mat)
-print(post_color[0][0])
+#.....................................................................
 
-createSquareMatrix(4, 12, post_color, force_color, stations, ind)
+#genetic algorithm test
+pop = createPopulation(50, 4, 12)
+
+pop_scores = []
+#score each pop
+min_score = 100000000
+min_index = 0
+for index in range(0, len(pop)):
+	seq_scores, ind_score = fitness(pop[index]['ind'], stations, wp_dict)
+	# print(seq_scores)
+	pop[index]['score'] = ind_score
+	if(ind_score < min_score):
+		min_score = ind_score
+		min_index = index
+
+# post_mat, post_color = createScoreMatrixANDColor(post_score, ind)
+# force_mat, force_color = createScoreMatrixANDColor(force_score, ind)
+
+all_colors = createWorkplaceMatrix(risk_factors)
 
 
-# sts = [stations[i] for i in seq]
-# sts2 = [stations[i] for i in seq2]
-# sts3 = [stations[i] for i in seq3]
-#
-# print(fitnessSeq(sts, wp_dict))
-# print(fitnessSeq(sts2, wp_dict))
-# print(fitnessSeq(sts3, wp_dict))
+#create square matrix of the best scored individual
+createSquareMatrix(4, 12, all_colors, stations, pop[min_index]['ind'], score_per_seq)
+
+#team_leader matrix
+createSquareMatrix(4, 12, all_colors, stations, load_TL_example(), score_per_seq_tl)
+
+plt.show()
+
+print(pop)
+worst_pop = get_best_pop(pop)
+ind_worse = pop[worst_pop]['ind']
+
+#try the genetic algorithm
+n = 1
+iterations=1
+m_score = [meanScores(pop)]
+previous_score = meanScores(pop)
+x_data = [0]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+Ln, = ax.plot(x_data, m_score)
+plt.ion()
+nbr_repeatedValues = 0
+
+while(n<30):
+	#fitness population
+	pop = fitnesspop(pop, stations, wp_dict)
+	#tournament selection
+	winners = TournamentSelection(pop, 10, 2)
+	#crossover
+	mat1, mat2 = crossover(pop[winners[0]], pop[winners[1]], 12)
+	#fitness of new elements
+	val_n, mat1_score = fitness(mat1, stations, wp_dict)
+	val_n, mat2_score = fitness(mat2, stations, wp_dict)
+	#check if scores are good
+	pop = newpop(mat1, mat1_score, mat2, mat2_score, pop)
+	#get mean scores
+	m_score.append(meanScores(pop))
+	x_data.append(iterations)
+	Ln.set_xdata(x_data)
+	Ln.set_ydata(m_score)
+	#increase loopping round
+	n+=1
+	iterations+=1
+	ax.set_xlim(0, iterations+10)
+	ax.set_ylim(min(m_score)-10, max(m_score)+10)
+	plt.pause(.01)
+	plt.xlabel("Nbr of Cycles")
+	plt.ylabel("Mean Score of Population")
+	if(m_score[-2]==m_score[-1]):
+		nbr_repeatedValues+=1
+	if(nbr_repeatedValues > 15):
+		n = 1
+		pop = createPopulation(50, 4, 12)
+		current_score = meanScores(pop)
+		nn = 0
+		while(current_score>previous_score or nn<10):
+			pop = createPopulation(50, 4, 12)
+			current_score = meanScores(pop)
+			nn+=1
+		previous_score = current_score
+		nbr_repeatedValues=0
+
+plt.show(block=True)
+
+best_pop = get_best_pop2(pop)
+best_ind = pop[best_pop]['ind']
+
+
+score_per_seq_worse, ind_score = fitness(ind_worse, stations, wp_dict)
+score_per_seq_best, ind_score = fitness(best_ind, stations, wp_dict)
+createSquareMatrix(4, 12, all_colors, stations, ind_worse, score_per_seq_worse)
+createSquareMatrix(4, 12, all_colors, stations, best_ind, score_per_seq_best)
+
+plt.show(block=True)
+
+
 
